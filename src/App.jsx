707bf7b00ddc9c76fd14f3f6adb01fd6a6ce98b0
mdnriyas17 <mdnriyas@gmail.com>
2023-08-24 13:38:10 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+
+
+const socket = io.connect("https://chat-app-backend-sc0d.onrender.com");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+       <h1 className="text-center p-2 bg-primary border-light text-light rounded">FUN CHAT APP</h1>
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="Name..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default App;
